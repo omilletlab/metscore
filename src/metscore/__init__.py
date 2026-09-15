a7e480b3efbe896a66fa1c50f72749b70c5be022
Rubén Gil-Redondo <rgil@cicbiogune.es>
2026-09-15 12:59:29 +0200

    Expose public prediction API

--- src/metscore/__init__.py
+++ src/metscore/__init__.py
@@ -1,2 +1,8 @@
-def hello() -> str:
-    return "Hello from metscore!"
+"""Python implementation of MetSCORE."""
+
+from metscore.core import PredictionResult, predict_array
+
+__all__ = [
+    "PredictionResult",
+    "predict_array",
+]
